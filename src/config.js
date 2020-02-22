51c3b384a6f9ffb3ca99272eb14a9a2022b779c3
Charlie Kim <cwkim3@gmail.com>
2020-02-21 22:32:02 -0500

    updated config.js

--- src/config.js
+++ src/config.js
@@ -1,11 +1,10 @@
 module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || 'development',
-  TWILIO_ACCOUNT_SID: process.env.TWILIO_ACCOUNT_SID || 'AC99a0268e1d9b0b8136ad052e9c6530a2',
-  TWILIO_AUTH_TOKEN: process.env.TWILIO_AUTH_TOKEN || '4428630b95c49cb61200eecceb4f5647',
-  TWILIO_PHONE_NUMBER: process.env.TWILIO_PHONE_NUMBER || '[+][1][8647540144]',
-  DATABASE_URL:
-    process.env.DATABASE_URL || 'postgresql://dunder_mifflin@localhost/metrodrycleaners',
-  JWT_SECRET: process.env.JWT_SECRET || 'change-this-secret',
-  JWT_EXPIRY: process.env.JWT_EXPIRY || '20s'
+  TWILIO_ACCOUNT_SID: process.env.TWILIO_ACCOUNT_SID,
+  TWILIO_AUTH_TOKEN: process.env.TWILIO_AUTH_TOKEN,
+  TWILIO_PHONE_NUMBER: process.env.TWILIO_PHONE_NUMBER,
+  DATABASE_URL: process.env.DATABASE_URL,
+  JWT_SECRET: process.env.JWT_SECRET,
+  JWT_EXPIRY: process.env.JWT_EXPIRY
 };
